df1869d5020728841c5a54a810a86b3c4a112d5a
Veselin Martionv <veselin.martinov@gmail.com>
2024-11-26 03:37:00 +0100

    add load signal

--- mama_to_be/profiles/apps.py
+++ mama_to_be/profiles/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class ProfilesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'mama_to_be.profiles'
+
+    def ready(self):
+        import mama_to_be.profiles.signals
