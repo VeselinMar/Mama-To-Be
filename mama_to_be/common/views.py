55b7e3889eddbde33c0e7f99e95ae95a4fe45417
Veselin Martionv <veselin.martinov@gmail.com>
2024-11-27 22:23:42 +0100

    Rework home view

--- mama_to_be/common/views.py
+++ mama_to_be/common/views.py
@@ -1,7 +1,24 @@
 from django.shortcuts import render
 
+from mama_to_be.articles.models import Article
+
+
 # Create your views here.
 
 
 def home(request):
-    return render(request, 'common/home.html')
+    recent_articles = (
+        Article.objects.filter(is_published=True)
+        .order_by('-published_at')[:12]
+    )
+
+    articles_by_category = {}
+    for article in recent_articles:
+        if article.category not in articles_by_category:
+            articles_by_category[article.category] = []
+        articles_by_category[article.category].append(article)
+
+    context = {
+        'articles_by_category': articles_by_category,
+    }
+    return render(request, 'common/home.html', context)
