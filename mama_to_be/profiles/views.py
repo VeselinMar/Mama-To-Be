91ab056f3dd954d3ba192a3f2857642c972a5ff0
Veselin Martionv <veselin.martinov@gmail.com>
2024-11-27 22:24:18 +0100

    Rework profile display view

--- mama_to_be/profiles/views.py
+++ mama_to_be/profiles/views.py
@@ -7,6 +7,7 @@ from django.urls import reverse_lazy
 from django.views import View
 from django.views.generic import FormView, UpdateView, DetailView
 
+from mama_to_be.articles.models import Article
 from mama_to_be.profiles.forms import RegisterForm, ProfileEditForm
 from mama_to_be.profiles.models import Profile
 
@@ -59,13 +60,36 @@ class ProfileEditView(LoginRequiredMixin, UpdateView):
         return Profile.objects.get(user=self.request.user)
 
 
-class ProfileDisplayView(DetailView):
+class ProfileDisplayView(LoginRequiredMixin, DetailView):
     model = Profile
     template_name = 'profiles/profile_details.html'
     context_object_name = 'profile'
 
-    def get_object(self):
-        username = self.kwargs.get('username')
+    def get_object(self, queryset=None):
+        # Check if username is provided in the URL, else fetch the profile for the logged-in user
+        username = self.kwargs.get('username', None)
+
         if username:
-            return get_object_or_404(Profile, username=username)
-        return get_object_or_404(Profile, user=self.request.user)
+            # Fetch the Profile based on the username of the related User model
+            return get_object_or_404(Profile, user__username=username)
+        else:
+            # If no username, get the profile of the logged-in user
+            return get_object_or_404(Profile, user=self.request.user)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        profile = self.get_object()
+        user = profile.user
+
+        # Get the last 3 published articles for this user
+        published_articles = Article.objects.filter(author=user, is_published=True).order_by('-published_at')[:3]
+
+        # Include unpublished articles if the profile belongs to the logged-in user
+        unpublished_articles = None
+        if user == self.request.user:
+            unpublished_articles = Article.objects.filter(author=user, is_published=False)
+
+        context['published_articles'] = published_articles
+        context['unpublished_articles'] = unpublished_articles
+        return context
