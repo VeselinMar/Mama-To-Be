59eccc2795504407c75c0119ba1d648231802422
Veselin Martionv <veselin.martinov@gmail.com>
2024-11-30 19:29:44 +0100

    remove loginrequired mixin from ProfileDisplayView, anonymous users should also be able to see user profiles

--- mama_to_be/profiles/views.py
+++ mama_to_be/profiles/views.py
@@ -17,7 +17,7 @@ from mama_to_be.profiles.models import Profile
 class RegisterView(FormView):
     template_name = "profiles/register.html"
     form_class = RegisterForm
-    success_url = reverse_lazy("edit")
+    success_url = reverse_lazy("profile-edit")
 
     def form_valid(self, form):
         # Save the user and log them in
@@ -53,25 +53,25 @@ class ProfileEditView(LoginRequiredMixin, UpdateView):
     model = Profile
     form_class = ProfileEditForm
     template_name = 'profiles/profile_update.html'
-    success_url = reverse_lazy('home')
+    success_url = reverse_lazy('profile-display_own')
 
     def get_object(self, queryset=None):
         # Ensure the logged-in user can only edit their own profile
         return Profile.objects.get(user=self.request.user)
 
 
-class ProfileDisplayView(LoginRequiredMixin, DetailView):
+class ProfileDisplayView(DetailView):
     model = Profile
     template_name = 'profiles/profile_details.html'
     context_object_name = 'profile'
 
     def get_object(self, queryset=None):
         # Check if username is provided in the URL, else fetch the profile for the logged-in user
-        username = self.kwargs.get('username', None)
+        user_id = self.kwargs.get('user_id', None)
 
-        if username:
+        if user_id:
             # Fetch the Profile based on the username of the related User model
-            return get_object_or_404(Profile, user__username=username)
+            return get_object_or_404(Profile, user__id=user_id)
         else:
             # If no username, get the profile of the logged-in user
             return get_object_or_404(Profile, user=self.request.user)
