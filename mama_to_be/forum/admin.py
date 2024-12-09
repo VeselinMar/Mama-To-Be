6901668ee22588e286ce4138544e1adecbc55322
Veselin Martionv <veselin.martinov@gmail.com>
2024-12-09 20:54:09 +0100

    Register admins for Category, Topic, Discussion, Comment and Like

--- mama_to_be/forum/admin.py
+++ mama_to_be/forum/admin.py
@@ -1,3 +1,44 @@
 from django.contrib import admin
 
+from mama_to_be.forum.models import Category, Topic, Discussion, Comment, Like
+
+
 # Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    search_fields = ('name',)
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(Topic)
+class TopicAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'created_by', 'created_at')
+    list_filter = ('category', 'created_at')
+    readonly_fields = ('created_by',)
+    search_fields = ('title', 'category__name', 'created_by__username')
+    prepopulated_fields = {'slug': ('title',)}
+
+
+@admin.register(Discussion)
+class DiscussionAdmin(admin.ModelAdmin):
+    list_display = ('content', 'created_by', 'created_at')
+    search_fields = ('content', 'created_by__username')
+    readonly_fields = ('created_by',)
+    list_filter = ('created_at',)
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('content', 'discussion', 'created_by', 'parent', 'created_at', 'likes')
+    search_fields = ('content', 'created_by__username')
+    list_filter = ('created_at', 'likes')
+    raw_id_fields = ('parent',)  # Makes it easier to select parent comments
+
+
+@admin.register(Like)
+class LikeAdmin(admin.ModelAdmin):
+    list_display = ('comment', 'user')
+    search_fields = ('comment__content', 'user__username')
+
