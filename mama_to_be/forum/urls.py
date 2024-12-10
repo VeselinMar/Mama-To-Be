77ce0264dab158a804041b1db93d0fc841a7a296
Veselin Martionv <veselin.martinov@gmail.com>
2024-12-10 22:09:13 +0100

    add TopicEditView

--- mama_to_be/forum/urls.py
+++ mama_to_be/forum/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path
 
 from mama_to_be.forum.views import ForumCategoryListView, TopicListView, TopicDetailView, \
-    create_topic, CreateCommentView, DiscussionDetailView, like_comment, ReplyCommentView, CategoryUpdateView
+    create_topic, CreateCommentView, DiscussionDetailView, like_comment, ReplyCommentView, CategoryUpdateView, \
+    TopicEditView
 
 urlpatterns = [
     path('', ForumCategoryListView.as_view(), name='category-list'),
-    path('category/<slug:category_slug>/', TopicListView.as_view(), name='topic-list'),
     path('categories/<int:pk>/edit/', CategoryUpdateView.as_view(), name='category-edit'),
+    path('category/<slug:category_slug>/', TopicListView.as_view(), name='topic-list'),
     path('topic/<int:pk>/', TopicDetailView.as_view(), name='topic-detail'),
+    path('topic/<int:pk>/edit/', TopicEditView.as_view(), name='topic-edit'),
     path('create-topic/', create_topic, name='create-topic'),
     path('discussion/<int:pk>/', DiscussionDetailView.as_view(), name='discussion-detail'),
     path('discussion/<int:discussion_id>/add-comment/', CreateCommentView.as_view(), name='create-comment'),
