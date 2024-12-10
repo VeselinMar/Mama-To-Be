ef4a4af770c8f68e9c9a6ac77dbab15c3386e20d
Veselin Martionv <veselin.martinov@gmail.com>
2024-12-10 23:35:19 +0100

    add uniqueness guarantee in save method

--- mama_to_be/articles/models.py
+++ mama_to_be/articles/models.py
@@ -1,3 +1,5 @@
+import uuid
+
 from django.db import models
 from django.utils.text import slugify
 from tinymce.models import HTMLField
@@ -50,8 +52,20 @@ class Article(models.Model):
         return self.title
 
     def save(self, *args, **kwargs):
+        # Generate the slug from the title if it's empty
         if not self.slug:
             self.slug = slugify(self.title)
+
+        # Ensure the slug is unique
+        original_slug = self.slug
+        queryset = Article.objects.filter(slug=self.slug)
+        if self.pk:
+            queryset = queryset.exclude(pk=self.pk)  # Exclude the current instance if it's being updated
+
+        # If a duplicate exists, generate a unique slug
+        if queryset.exists():
+            self.slug = f"{original_slug}-{uuid.uuid4().hex[:8]}"  # Append a UUID to ensure uniqueness
+
         super().save(*args, **kwargs)
 
     class Meta:
