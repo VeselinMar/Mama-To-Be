8a5dfbf6767e51f0feb61ad31b5175987859682c
Veselin Martionv <veselin.martinov@gmail.com>
2024-12-10 20:56:57 +0100

    add CategoryUpdateView

--- mama_to_be/forum/urls.py
+++ mama_to_be/forum/urls.py
@@ -1,15 +1,16 @@
 from django.urls import path
 
 from mama_to_be.forum.views import ForumCategoryListView, TopicListView, TopicDetailView, \
-    create_topic, CreateCommentView, DiscussionDetailView, like_comment, ReplyCommentView
+    create_topic, CreateCommentView, DiscussionDetailView, like_comment, ReplyCommentView, CategoryUpdateView
 
 urlpatterns = [
     path('', ForumCategoryListView.as_view(), name='category-list'),
     path('category/<slug:category_slug>/', TopicListView.as_view(), name='topic-list'),
+    path('categories/<int:pk>/edit/', CategoryUpdateView.as_view(), name='category-edit'),
     path('topic/<int:pk>/', TopicDetailView.as_view(), name='topic-detail'),
+    path('create-topic/', create_topic, name='create-topic'),
+    path('discussion/<int:pk>/', DiscussionDetailView.as_view(), name='discussion-detail'),
     path('discussion/<int:discussion_id>/add-comment/', CreateCommentView.as_view(), name='create-comment'),
     path('comment/<int:comment_id>/reply/', ReplyCommentView.as_view(), name='reply-comment'),
     path('comment/<int:comment_id>/like/', like_comment, name='like-comment'),
-    path('create-topic/', create_topic, name='create-topic'),
-    path('discussion/<int:pk>/', DiscussionDetailView.as_view(), name='discussion-detail'),
 ]
