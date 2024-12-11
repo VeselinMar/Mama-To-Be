dcc4034f78c4af8b29c371c522246fdc8f6e5449
Veselin Martionv <veselin.martinov@gmail.com>
2024-12-11 21:14:48 +0100

    clean imports

--- mama_to_be/profiles/models.py
+++ mama_to_be/profiles/models.py
@@ -1,8 +1,6 @@
-from django.contrib.auth import get_user_model
 from django.contrib.auth.base_user import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
 from django.db import models
-from django.forms import JSONField
 from django.utils.timezone import now
 
 from mama_to_be.profiles.managers import AppUserManager
