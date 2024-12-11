017cc422b60ac19ee2c48ac958996b7fd0a45266
Veselin Martionv <veselin.martinov@gmail.com>
2024-12-11 17:25:11 +0100

    update CategoryArticlesView -rework as CBV -update urls

--- mama_to_be/articles/urls.py
+++ mama_to_be/articles/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from mama_to_be.articles.views import ArticleCreateView, ArticleDisplayView, ArticleEditView, category_articles
+from mama_to_be.articles.views import (ArticleCreateView, ArticleDisplayView,
+                                       ArticleEditView, CategoryArticlesView)
 
 urlpatterns = [
     path('create/', ArticleCreateView.as_view(), name='create-article'),
     path('<slug:slug>/', ArticleDisplayView.as_view(), name='article-detail'),
     path('<slug:slug>/edit', ArticleEditView.as_view(), name='article-edit'),
-    path('category/<str:category>/', category_articles, name='category-articles'),
+    path('category/<str:category>/', CategoryArticlesView.as_view(), name='category-articles'),
 
 ]
--- mama_to_be/articles/views.py
+++ mama_to_be/articles/views.py
@@ -1,9 +1,7 @@
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.core.paginator import Paginator
-from django.http import HttpResponseForbidden
-from django.shortcuts import redirect, render
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.http import Http404
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, UpdateView, DetailView
+from django.views.generic import CreateView, UpdateView, DetailView, ListView
 
 from mama_to_be.articles.choices import CategoryChoices
 from mama_to_be.articles.forms import ArticleForm
@@ -14,11 +12,15 @@ from mama_to_be.profiles.models import Profile
 # Create your views here.
 
 class ArticleCreateView(LoginRequiredMixin, CreateView):
+    """
+    View to create a new article. Only accessible to logged-in users.
+    """
     model = Article
     form_class = ArticleForm
     template_name = 'articles/create_article_form.html'
     success_url = reverse_lazy('home')
     login_url = reverse_lazy('login')
+    success_message = "Article created successfully!"
 
     def form_valid(self, form):
         form.instance.author = self.request.user
@@ -26,25 +28,27 @@ class ArticleCreateView(LoginRequiredMixin, CreateView):
         return super().form_valid(form)
 
 
-class ArticleEditView(UpdateView):
+class ArticleEditView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    """
+    View to edit an existing article. Only accessible to author.
+    """
     model = Article
     form_class = ArticleForm
     template_name = 'articles/create_article_form.html'
     context_object_name = 'article'
 
-    def get_success_url(self):
-        # Redirect to the article detail page after successful editing
-        return reverse_lazy('article-detail', kwargs={'slug': self.object.slug})
-
-    def dispatch(self, request, *args, **kwargs):
-        # Restrict access to the article author only
+    def test_func(self):
         article = self.get_object()
-        if article.author != request.user:
-            return HttpResponseForbidden("You are not allowed to edit this article.")
-        return super().dispatch(request, *args, **kwargs)
+        return article.author == self.request.user
+
+    def get_success_url(self):
+        return reverse_lazy('article-detail', kwargs={'slug': self.object.slug})
 
 
 class ArticleDisplayView(DetailView):
+    """
+    View to display an article. Freely accessible.
+    """
     model = Article
     template_name = 'articles/article_detail.html'
     context_object_name = 'article'
@@ -59,21 +63,25 @@ class ArticleDisplayView(DetailView):
         return context
 
 
-def category_articles(request, category):
-    if category not in CategoryChoices.values:
-        # Handle invalid category (e.g., return a 404 error or redirect)
-        return redirect('home')
+class CategoryArticlesView(ListView):
+    model = Article
+    template_name = 'articles/category_articles.html'
+    context_object_name = 'object_list'  # Default name for the queryset
+    paginate_by = 9
 
-    # Fetch articles of the given category
-    articles = Article.objects.filter(category=category, is_published=True).order_by('-published_at')
+    def get_queryset(self):
+        # Validate the category from the URL
+        category = self.kwargs.get('category')
+        if category not in CategoryChoices.values:
+            raise Http404("Invalid category")  # Return 404 for invalid category
 
-    # Paginate the articles (5 per page)
-    paginator = Paginator(articles, 5)
-    page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
+        # Use the custom manager's `in_category` method to fetch published articles in this category
+        return Article.objects.in_category(category)
 
-    context = {
-        'category': category,
-        'page_obj': page_obj,
-    }
-    return render(request, 'articles/category_articles.html', context)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Add the category to the context for use in the template
+        context['category'] = self.kwargs['category']
+        # Ensure page_obj is explicitly passed to the context
+        context['page_obj'] = context.get('page_obj', context['object_list'])
+        return context
