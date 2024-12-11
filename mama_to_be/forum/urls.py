bce3d64a124b06f14969892ad9f7f40773d9ca62
Veselin Martionv <veselin.martinov@gmail.com>
2024-12-11 22:29:09 +0100

    delete obsolete paths

--- mama_to_be/forum/urls.py
+++ mama_to_be/forum/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from mama_to_be.forum.views import ForumCategoryListView, TopicListView, TopicDetailView, \
-    create_topic, CreateCommentView, DiscussionDetailView, like_comment, ReplyCommentView, CategoryUpdateView, \
-    TopicEditView, EditCommentView, EditDiscussionView
+from mama_to_be.forum.views import (ForumCategoryListView, TopicListView, TopicDetailView,
+                                    CreateCommentView, DiscussionDetailView, like_comment,
+                                    ReplyCommentView, CategoryUpdateView,
+                                    TopicEditView, EditCommentView, EditDiscussionView)
 
 urlpatterns = [
     path('', ForumCategoryListView.as_view(), name='category-list'),
@@ -10,7 +11,6 @@ urlpatterns = [
     path('category/<slug:category_slug>/', TopicListView.as_view(), name='topic-list'),
     path('topic/<int:pk>/', TopicDetailView.as_view(), name='topic-detail'),
     path('topic/<int:pk>/edit/', TopicEditView.as_view(), name='topic-edit'),
-    path('create-topic/', create_topic, name='create-topic'),
     path('discussion/<int:pk>/', DiscussionDetailView.as_view(), name='discussion-detail'),
     path('discussion/<int:discussion_id>/add-comment/', CreateCommentView.as_view(), name='create-comment'),
     path('discussion/<int:pk>/edit/', EditDiscussionView.as_view(), name='edit-discussion'),
