842a511e9410069135ceaebe381473bb97528c5f
Veselin Martionv <veselin.martinov@gmail.com>
2025-01-03 09:57:14 +0100

    hide sensitive info

--- mama_to_be/settings.py
+++ mama_to_be/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
+from decouple import config
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-f2cxq-3k84)##@pqzvso3_&wzfl+z!)#dltxicue&2hbly=spe'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -83,10 +85,10 @@ WSGI_APPLICATION = 'mama_to_be.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "mama_db",
-        "USER": "postgres",
-        "PASSWORD": "Veselin4o",
-        "HOST": "127.0.0.1",
+        "NAME": config('DB_NAME'),
+        "USER": config('DB_USER'),
+        "PASSWORD": config('DB_PASSWORD'),
+        "HOST": config('DB_HOST'),
         "PORT": "5432",
     }
 }
