1357ae75e0cfc09617238ba80b9444c79c0a1e62
VeselinMar <veselin.martinov@gmail.com>
2025-08-30 17:41:22 +0200

    butcher search engine

--- mama_to_be/articles/signals.py
+++ mama_to_be/articles/signals.py
@@ -1,16 +1,16 @@
+from django.config import settings
 from django.contrib.postgres.search import SearchVector
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 from mama_to_be.common.signals import dump_seed
 from mama_to_be.common.github import schedule_commit
 
-
 from mama_to_be.articles.models import Article
 
 
 @receiver(post_save, sender=Article)
 def update_search_vector_on_save(sender, instance, **kwargs):
-    if 'postgres' in settings.DATABASE['default']['ENGINE']:
+    if 'postgres' in settings.DATABASES['default']['ENGINE']:
         instance.search_vector = (
             SearchVector('title', weight='A') + SearchVector('content', weight='B')
         )
