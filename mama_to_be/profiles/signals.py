9e1773795e76004e534646ffd3e92903d9815ac4
VeselinMar <veselin.martinov@gmail.com>
2025-09-08 14:20:35 +0200

    restructure

--- mama_to_be/profiles/signals.py
+++ mama_to_be/profiles/signals.py
@@ -3,7 +3,7 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from mama_to_be.common.signals import dump_seed
 from mama_to_be.common.github import commit_seed_to_github
-from mama_to_be.common.utils.signal_control import DISABLE_SEED_SIGNALS
+from mama_to_be.common import utils
 from mama_to_be.profiles.models import Profile
 
 UserModel = get_user_model()
@@ -11,14 +11,14 @@ UserModel = get_user_model()
 
 @receiver(post_save, sender=UserModel)
 def create_profile(sender, instance, created, **kwargs):
-    if DISABLE_SEED_SIGNALS:
+    if utils.signal_control.DISABLE_SEED_SIGNALS:
         return
     if created:
         Profile.objects.update_or_create(user=instance)
 
-@receiver([post_save], sender=UserModel)
+@receiver(post_save, sender=UserModel)
 def user_changed(sender, **kwargs):
-    if DISABLE_SEED_SIGNALS:
+    if utils.signal_control.DISABLE_SEED_SIGNALS:
         return
     dump_seed()
     commit_seed_to_github()
