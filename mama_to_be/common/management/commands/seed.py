d28cb5e700e31fdd18bbd901892aa3e39d6315fc
VeselinMar <veselin.martinov@gmail.com>
2025-09-08 14:11:33 +0200

    rework seed logic

--- mama_to_be/common/management/commands/seed.py
+++ mama_to_be/common/management/commands/seed.py
@@ -1,7 +1,6 @@
 from django.core.management import call_command
 from django.core.management.base import BaseCommand
 from mama_to_be.common.utils.signal_control import disable_seed_signals
-from mama_to_be.common.utils import signal_control
 
 class Command(BaseCommand):
     help = "Seed initial data into the database"
@@ -9,10 +8,7 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         self.stdout.write(self.style.WARNING("Seeding DB..."))
 
-        signal_control.DISABLE_SEED_SIGNALS = True
-        try:
+        with disable_seed_signals():
             call_command("loaddata", "seed.json")
-        finally:
-            signal_control.DISABLE_SEED_SIGNALS = False
         
         self.stdout.write(self.style.SUCCESS("Seed complete."))
