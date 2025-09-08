0689c7c413f0ad36b034744547042af6c48f9b54
VeselinMar <veselin.martinov@gmail.com>
2025-09-09 00:14:26 +0200

    add profile creation manager

--- mama_to_be/settings.py
+++ mama_to_be/settings.py
@@ -130,7 +130,7 @@ STATIC_URL = 'static/'
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-STATICFILES_DIRS = BASE_DIR / "static",
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 MEDIA_URL = '/mediafiles/'
 
