c28c47487a6b91e31ba0fad9a680a20ca8323ec1
VeselinMar <veselin.martinov@gmail.com>
2026-02-28 22:55:23 +0100

    remove obsolete import statement and extend settings

--- mama_to_be/settings.py
+++ mama_to_be/settings.py
@@ -35,8 +35,17 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+    'mom-to-be.at',
+    'mom-to-be.onrender.com',
+    ]
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://mom-to-be.at',
+    'https://mom-to-be.onrender.com',
+    ]
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Application definition
 
