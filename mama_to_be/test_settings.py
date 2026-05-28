f85ae7f8bf9764cd8313ef261c017e5a222d7011
VeselinMar <veselin.martinov@gmail.com>
2026-05-28 13:46:18 +0200

    extend settings and test_settings for ci pipeline

--- mama_to_be/test_settings.py
+++ mama_to_be/test_settings.py
@@ -52,3 +52,17 @@ CACHES = {
 # --------------------------------------------------
 
 RATELIMIT_ENABLE = False
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "INFO",
+    },
+}
